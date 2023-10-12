c58445bd5e2404b9e85c8e96ce368d5853176ed2
Caio-Santana <caio.santana@live.com>
2023-10-12 13:51:59 -0300

    First commit

---
+++ app/src/main/java/com/sam/catchat/DraftsFragment.java
@@ -0,0 +1,22 @@
+package com.sam.catchat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DraftsFragment extends Fragment {
+
+    public DraftsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_drafts, container, false);
+    }
+}
